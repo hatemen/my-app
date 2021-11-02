3869ee56d1b5773f29748c9f0bf568046e516ea9
ziedbouf <zied.boufaden@gmail.com>
2021-11-02 14:53:09 +0100

    add static directory to django config, MEDIA and STATIC folders

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -68,10 +68,24 @@ TEMPLATES = [
             ],
         },
     },
+    
 ]
 
-WSGI_APPLICATION = 'myapp.wsgi.application'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+os.makedirs(MEDIA_ROOT, exist_ok=True)
+
+WSGI_APPLICATION = 'myapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
